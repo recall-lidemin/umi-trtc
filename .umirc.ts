5091ce0a9b41810bd72a699956899b4156c06fcd
recall-lidemin <1425889927@qq.com>
2021-01-17 19:32:40 +0800

    修改publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'umi'
 
+const BASE_PATH = '/web-rtc/'
+
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
+  },
+  publicPath: BASE_PATH,
+  base: BASE_PATH,
+  favicon: `${BASE_PATH}favicon.png`,
+  dynamicImport: {
+    loading: '@/components/PageLoading/index',
   },
 })
---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,12 @@
+import { PageLoading } from '@ant-design/pro-layout'
+import React from 'react'
+// eslint-disable-next-line import/export
+function Loading() {
+  return (
+    <div>
+      <PageLoading />
+    </div>
+  )
+}
+
+export default Loading
